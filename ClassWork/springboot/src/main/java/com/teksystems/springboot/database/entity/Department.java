81e2290872da05893d69bde97e68782fecced433
abp46 <abp46@pitt.edu>
2022-11-17 09:18:49 -0500

    springboot stuff

---
+++ ClassWork/springboot/src/main/java/com/teksystems/springboot/database/entity/Department.java
@@ -0,0 +1,41 @@
+package com.teksystems.springboot.database.entity;
+
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@EqualsAndHashCode
+@ToString
+@Table(name = "department")
+public class Department {
+
+	@Id
+	@Column(name = "id")
+	@EqualsAndHashCode.Include
+	private Integer id;
+
+	@Column(name = "name")
+	@EqualsAndHashCode.Exclude
+	private String name;
+
+//	@OneToMany(mappedBy = "department", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+//	private List<Course> courses;
+}
